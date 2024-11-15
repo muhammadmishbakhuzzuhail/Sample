36a1b6a5f5c1bd9e692f48d307923bda0cf243a6
Muhammad Mishbakhuz Zuhail <muhammadmishbakhuzzuhail@gmail.com>
2024-11-16 03:24:15 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: "build", // folder output build
+    },
+    base: "/React-1/", // Sesuaikan dengan nama repository kamu di GitHub
 });
